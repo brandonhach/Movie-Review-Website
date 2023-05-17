3d672aaaf3a662b94719325213ef3d797d4d32d6
brandonhach <specialcambo@gmail.com>
2023-05-17 15:45:27 -0400

    first commit

---
+++ src/main/java/com/example/movies/Review.java
@@ -0,0 +1,18 @@
+package com.example.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+
+}
